7fb305d6ed8121cfd9a9585bb5c30b3a42fc27b9
ZHAO Xudong <zxdong@gmail.com>
2018-06-24 23:27:51 +0800

    save

---
+++ README.md
@@ -0,0 +1,9 @@
+# github-api-handler-go
+
+## run
+```
+git clone git@github.com:electerm/github-api-handler-go.git
+cd github-api-handler-go
+cp config.default.toml config.toml
+go run app.go
+```
--- app.go
+++ app.go
@@ -3,22 +3,45 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
-	// "os"
+	"github.com/koding/multiconfig"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-	//main
+// Server is config interface
+type Server struct {
+	Port int `default:"7654"`
+}
+
+//Handler is request Handler
+func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
 
 func main() {
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/", Handler)
 
+	defaultConf := multiconfig.NewWithPath("config.default.toml")
+	localConf := multiconfig.NewWithPath("config.toml") // supports TOML and JSON
+	// Get an empty struct for your configuration
+	serverDefaultConf := new(Server)
+	serverUserConf := new(Server)
+
+	// Populated the serverConf struct
+	defaultConf.MustLoad(serverDefaultConf)
+	err := localConf.Load(serverUserConf) // Check for error
+	//m.MustLoad(serverConf) // Panic's if there is any error
 	// Bind to a port and pass our router in
-	log.Print("ddd")
-	log.Fatal(http.ListenAndServe("localhost:8000", r))
+	if err != nil {
+		log.Print("no config.toml, but it is ok")
+	} else {
+		serverDefaultConf.Port = serverUserConf.Port
+	}
+
+	addr := "localhost:" + strconv.Itoa(serverDefaultConf.Port)
+	log.Print("server runs on ", addr)
+	log.Fatal(http.ListenAndServe(addr, r))
 }
